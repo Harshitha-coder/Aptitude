4fc11e1f9a90a327ccdfe31fe94f26f0d2183876
Harshitha1308 <harshithak794@gmail.com>
2021-05-06 11:42:50 +0530

    add Games

---
+++ Games.java
@@ -0,0 +1,11 @@
+class Games{
+public static void main(String[] args){
+  char time='a';
+if(time!='a'){
+System.out.println("listening music");
+}
+else{
+System.out.println("playing games");
+}
+}
+}
