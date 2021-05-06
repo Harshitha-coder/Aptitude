239d5fe570c475c00d78239ddf49b186d0712bda
Harshitha1308 <harshithak794@gmail.com>
2021-05-06 11:36:50 +0530

    add Automobiles

---
+++ Automobiles.java
@@ -0,0 +1,11 @@
+class Automobiles{
+public static void main(String[] args){
+  double control=7.0;
+if(control++>=--control){
+System.out.println("Watching movies");
+}
+else{
+System.out.println("Riding bike");
+}
+}
+}
