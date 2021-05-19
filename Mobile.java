4100996e11f3c5db3b07c3c9defc04d9fc676a77
Harshitha1308 <harshithak794@gmail.com>
2021-05-19 12:30:48 +0530

    add Mobile

---
+++ Mobile.java
@@ -0,0 +1,24 @@
+class Mobile{
+public static void main(String[] args){
+  String model=" 9pro";
+  int memory=128;
+if(model == " 9pro"){
+System.out.println("oneplus 9pro");
+if(memory== 64){
+System.out.println("oneplus 9pro 64gb");
+}
+ else if(memory==128){
+System.out.println("oneplus 9pro 128gb");
+}
+}
+else if(model == " 9pro"){
+System.out.println("oneplus 9pro");
+if(memory== 64){
+System.out.println("oneplus 9pro 64gb");
+}
+ else if(memory==128){
+System.out.println("oneplus 9pro 128gb");
+}
+}
+}
+}
