b4d5decab7dfa8f57853f844ac9b67c1dd5ec14f
Harshitha1308 <harshithak794@gmail.com>
2021-05-19 12:28:36 +0530

    add Laptop

---
+++ Laptop.java
@@ -0,0 +1,21 @@
+class Laptop{
+public static void main(String[] args){
+
+switch("dell"){
+case "hp" :System.out.println("hp laptop");
+            break;
+case "lenovo" :System.out.println("lenovo laptop");
+                break;
+case "dell" :System.out.println("dell laptop");
+              break;
+case "mac" :System.out.println("mac laptop");
+             break;
+case "asus" :System.out.println("asus laptop");
+              break;
+default:System.out.println("laptop not available");
+              break;
+}
+
+}
+}
+
