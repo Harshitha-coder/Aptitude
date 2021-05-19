488d28187bdd4626215ffacf3ee6ceb5c1731f79
Harshitha1308 <harshithak794@gmail.com>
2021-05-19 12:36:03 +0530

    add Numbers

---
+++ Numbers.java
@@ -0,0 +1,20 @@
+class Numbers{
+public static void main(String[] args){
+   for(int num=1; num<=10; num++)
+{
+System.out.println("Helloworld" +num);
+}
+String name = "Harshitha";
+while(name != "xyzz"){
+System.out.println("Welcome" +name);
+name = "xyzz";
+}
+
+ name = "Harshitha";
+do{
+System.out.println("Welcome" +name);
+name = "xyzz";
+}while(name!="xyzz");
+
+}
+}
