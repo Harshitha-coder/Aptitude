20dd2a944088c9ae40835c25a4e0186fff68ccf3
Harshitha1308 <harshithak794@gmail.com>
2021-05-19 12:27:29 +0530

    add Battle

---
+++ Battle.java
@@ -0,0 +1,12 @@
+class Battle{
+public static void main(String[] args){
+ 
+ Boolean value = true; 
+if(value){
+System.out.println("listening music while cooking");
+}
+else{
+System.out.println("playing games");
+}
+}
+}
