62913d238d143b7c4190d47e9c5ef2bfc1ebec52
Harshitha1308 <harshithak794@gmail.com>
2021-05-19 12:31:52 +0530

    add Motors

---
+++ Motors.java
@@ -0,0 +1,13 @@
+class Motors{
+public static void main(String[] args){
+  int control1=7;
+  float control2=5.0f;
+
+if(++control1<=control2-- ? true : false){
+System.out.println("Motor is running");
+}
+else{
+System.out.println("Motor is not running");
+}
+}
+}
